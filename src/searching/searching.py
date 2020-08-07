5d4e54fb91bc02fadd8ea0b883d4ea07f475ed9e
Jimmique <jimmique88@gmail.com>
2020-08-06 22:40:44 -0700

    Completed Binary Search for MVP

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -11,5 +11,25 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
+    if len(arr) == 0:
+        return -1  # array empty
+
+    low = 0
+    high = len(arr)-1
+
+    while low <= high:
+        # go to the middle
+        middle = (low + high)//2
+
+        # check if the middle is less than or greater than our target
+        # if less, eliminate the right-hand side
+        if target < arr[middle]:
+            high = middle - 1
+
+        elif target > arr[middle]:
+            low = middle + 1
+
+        else:
+            return middle
 
     return -1  # not found
